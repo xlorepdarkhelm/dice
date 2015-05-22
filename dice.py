4e157bf8f2fba8a7dd62f8966ddda5b17bd80d96
xlorepdarkhelm <xlorep@darkhelm.org>
2015-05-22 16:16:56 +0000

    Working on DiceAdder

--- dice.py
+++ dice.py
@@ -15,6 +15,15 @@ def standard_dice(values):
     return sum(values)
 
 
+class HasConvention:
+    def __init__(self, convention):
+        self.__convention = convention
+
+    @property
+    def convention(self):
+        return self.__convention
+
+
 @functools.total_ordering
 class Rollable(collections.abc.Hashable, collections.abc.Callable, metaclass=abc.ABCMeta):
     @property
@@ -66,34 +75,39 @@ class Rollable(collections.abc.Hashable, collections.abc.Callable, metaclass=abc
     def copy(self):
         pass
 
+    def __add__(self, other):
+        return DiceAdder(self, other)
 
-class Die(Rollable):
+    def __radd__(self, other):
+        return DiceAdder(other, self)
+
+class Die(Rollable, HasConvention):
     def __init__(self, sides, convention=standard_die):
         sides = int(sides)
         if sides < 2:
             raise ValueError('There must be at least two sides.')
 
         self.__sides = sides
-        self.__convention = convention
+        HasConvention.__init__(self, convention)
 
     @property
     def sides(self):
         return self.__sides
 
     def _roll(self):
-        return self.__convention(random.randrange(1, self.sides + 1))
+        return self.convention(random.randrange(1, self.sides + 1))
 
     def copy(self):
         return Die(self.sides)
 
     def __hash__(self):
-        return hash((type(Die), self.sides))
+        return hash((type(Die), self.convention, self.sides))
 
     def __str__(self):
         return ''.join(['d', str(self.sides)])
 
     def __repr__(self):
-        return ''.join(['Die(', str(self.sides), ')'])
+        return ''.join(['Die(', repr(self.sides), ')'])
 
 class RollableSequence(collections.abc.Sequence, Rollable):
     def __init__(self, group=[]):
@@ -109,7 +123,7 @@ class RollableSequence(collections.abc.Sequence, Rollable):
     def __len__(self):
         return len(self._group)
 
-class Dice(RollableSequence):
+class Dice(RollableSequence, HasConvention):
     def __init__(self, num, rollable, convention=standard_dice):
         num = int(num)
         if num < 1:
@@ -117,22 +131,166 @@ class Dice(RollableSequence):
 
         if isinstance(rollable, Dice):
             num = num * len(rollable)
-            rollable = rollable.group[0].copy()
+            rollable = rollable.die
 
-        super().__init__((rollable.copy() for i in range(num)))
-        self.__convention = convention
+        RollableSequence.__init__(self, (rollable.copy() for i in range(num)))
+        HasConvention.__init__(self, convention)
+
 
     def _roll(self):
-        return self.__convention(item() for item in self._group)
+        return self.convention(item() for item in self._group)
+
+    @property
+    def die(self):
+        try:
+            return self.__die
+
+        except AttributeError:
+            self.__die = self._group[0].copy()
+            return self.__die
 
     def copy(self):
-        return Dice(len(self), self.group[0])
+        return Dice(len(self), self.die)
 
     def __hash__(self):
-        return hash((type(self), ) + tuple(self._group))
+        return hash((type(self), self.convention) + self._group)
 
     def __str__(self):
-        return ''.join([str(len(self)), str(self._group[0])])
+        return ''.join([str(len(self)), str(self.die)])
 
     def __repr__(self):
-        return ''.join(['Dice(', str(len(self)), ', ', repr(self._group[0]), ')'])
+        return ''.join(['Dice(', repr(len(self)), ', ', repr(self.die), ')'])
+
+class DiceAdder(RollableSequence):
+    def __new__(cls, *adders, scalar=0):
+
+        mappings = {
+            type_: [item for item in adders if type(item) is type_]
+            for type_ in {type(item) for item in adders}
+        }
+        while DiceAdder in mappings:
+            dice_adders = mappings.pop(DiceAdder)
+            adder_items, scalars = zip(*[
+                (
+                    adder._group,
+                    adder.scalar
+                )
+                for adder in dice_adders
+            ])
+            adder_items = tuple(item for group in adder_items for item in group)
+            scalar += sum(item_scalar for item_scalar in scalars)
+            for type_, items in (
+                (type_, [item for item in adder_items if type(item) is type_])
+                for type_ in {type(item) for item in adder_items}
+            ):
+                if type_ in mappings:
+                    mappings[type_].extend(items)
+
+                else:
+                    mappings[type_] = items
+
+        merged_adders = []
+        if Dice in mappings:
+            dice_items = mappings.pop(Dice)
+            items = {
+                dtype: sum(
+                    len(item)
+                    for item in dice_items
+                    if hash(item.die) == hash(dtype)
+                )
+                for dtype in {item.die for item in dice_items}
+            }
+        else:
+            items = {}
+
+        if Die in mappings:
+            die_items = mappings.pop(Die)
+
+            items.update({
+                dtype: items.get(dtype, 0) + count
+                for dtype, count in (
+                    (
+                        set_item,
+                        len([
+                            item
+                            for item in die_items
+                            if hash(item) == hash(set_item)
+                        ])
+                    )
+                    for set_item in set(die_items)
+                )
+            })
+
+        merged_adders.extend(
+            (Dice(count, die) if count > 1 else die)
+            for die, count in items.items()
+        )
+
+        if mappings:
+            rollable_items, scalars = zip(*[
+                (
+                    items if is_rollable else None,
+                    sum(items) if not is_rollable else None
+                )
+                for items, is_rollable in (
+                    (
+                        items,
+                        issubclass(type_, Rollable)
+                    )
+                    for type_, items in mappings.items()
+                    for item in items
+                )
+            ])
+
+            merged_adders.extend(
+                item
+                for group in (
+                    items
+                    for items in rollable_items
+                    if items is not None
+                )
+                for item in group
+            )
+            scalar += sum(item for item in scalars if item is not None)
+
+        if not scalar and len(merged_adders) == 1:
+            ret = merged_adders[0]
+
+        else:
+            ret = super().__new__(cls)
+            ret.__scalar = scalar
+            RollableSequence.__init__(ret, merged_adders)
+
+        return ret
+
+    def __init__(self, *adders, scalar=0):
+        pass
+
+    @property
+    def scalar(self):
+        return self.__scalar
+
+    def _roll(self):
+        return sum(item() for item in self._group) + self.scalar
+
+    def copy(self):
+        return DiceAdder(self.scalar, *self._group)
+
+    def __hash__(self):
+        return hash((type(self), self.scalar) + self._group)
+
+    def __str__(self):
+        ret = ' + '.join(str(item) for item in self._group)
+        if self.scalar > 0:
+            ret = ' + '.join([ret, str(self.scalar)])
+        elif self.scalar < 0:
+            ret = ' - '.join([ret, str(abs(self.scalar))])
+
+        return ret
+
+    def __repr__(self):
+        ret = ', '.join(repr(item) for item in self._group)
+        if self.scalar:
+            ret = ', '.join([ret, repr(self.scalar)])
+
+        return ''.join(['DiceAdder(', ret, ')'])
